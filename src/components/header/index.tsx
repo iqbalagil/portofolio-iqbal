0d0ec76f2a3ee0212ca21a6cab69c3bb95c03073
iqbalagil <iqbalagil@gmail.com>
2025-09-25 22:07:39 +0700

    update: fix the layout, but need revision on gsap animation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -12,43 +12,43 @@ export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     const container = useRef<HTMLDivElement>(null);
     const domComponent = {
-        burger: useRef<HTMLDivElement>(null),
-        link: useRef<HTMLAnchorElement>(null),
+        link: useRef<HTMLDivElement>(null),
         burgerLine: useRef<HTMLDivElement>(null),
-        burgerComponent: useRef<HTMLDivElement>(null)
     }
-
     const links = [
         {name: "About", href: "#about"},
         {name: "Work", href: "#work"},
         {name: "Social", href: "#social"},
     ]
+    const l = gsap.utils.selector(domComponent.link);
     const tl = useRef<gsap.core.Timeline>(null);
     useGSAP(() => {
         tl.current = gsap.timeline({paused: true})
-            .to(domComponent.burger.current, {
-                display: "flex",
-                flexDirection: "column",
+            .to(`.${styles.navLink}`, {
+                display: "grid",
                 width: 300,
                 height: 180,
-                flexWrap: "wrap",
                 borderRadius: "20px",
                 ease: "power4.out",
                 y: 80,
                 duration: 0.5
             })
-            .to(domComponent.link.current, {
-                duration: 0.5,
+            .to(l(`.${styles.text}`), {
+                duration: 1,
                 opacity: 1,
                 ease: "power4.out",
             }, "-=0.3")
-            .to(domComponent.burgerComponent.current, {
-                right: 1,
-                left: 0,
-                padding: "20px",
-                duration: 0.2,
-            }, "-=0.1");
-
+            .to(`.${styles.burgerLine}`, {
+                gridColumn: 1,
+                gridRow: 1,
+            })
+            .to(l(`.${styles.text}`), {
+                gridColumn: 2,
+                gridRow: 1,
+                duration: 1,
+                opacity: 1,
+                stagger: 0.5
+            });
 
     }, {scope: container});
 
@@ -61,44 +61,39 @@ export default function Navbar() {
     }, [isOpen]);
     return (
         <nav ref={container} className={styles.nav}>
-            <div ref={domComponent.burger} className={styles.navLink}>
+            <div className={styles.navLink}>
                 <div onClick={() => {
                     setIsOpen(!isOpen)
                 }} className={`${styles.burger} ${isOpen ? styles.burgerActive : ""}`}>
-                    <div ref={domComponent.burgerComponent} className={styles.burgerComponent}>
-
-                        <div ref={domComponent.burgerLine} className={styles.burgerLine}>
-                            <div className={styles.line1}/>
-                            <div className={styles.line2}/>
-                        </div>
-
-                        {
-                            isOpen ? (
-                                <div>
-                                    {
-                                        links.map((link) => (
-                                            <Link key={link.name} href={link.href} className={styles.text}
-                                                  ref={domComponent.link}>
-                                                {link.name}
-                                            </Link>
-                                        ))
-                                    }
-                                </div>
-                            ) : null
-                        }
+                    <div ref={domComponent.burgerLine} className={styles.burgerLine}>
+                        <div className={styles.line1}/>
+                        <div className={styles.line2}/>
                     </div>
 
-                </div>
+                    {isOpen ? (
+            <div ref={domComponent.link} className={styles.linksCenter}>
+              {links.map((link) => (
+                <Link
+                  key={link.name}
+                  href={link.href}
+                  className={styles.text}
+                  onClick={() => setIsOpen(false)}
+                >
+                  {link.name}
+                </Link>
+              ))}
             </div>
-            <div>
-                <button className={styles.iconBtn}/>
+          ) : null}
+        </div>
+      </div>
 
-            </div>
-            <button className={styles.navBtnLink}>
-                <span className={styles.text}>
-                    LET&apos;S TALK
-                </span>
-            </button>
-        </nav>
-    );
+      <div>
+        <button className={styles.iconBtn}/>
+      </div>
+
+      <button className={styles.navBtnLink}>
+        <span className={styles.text}>LET&apos;S TALK</span>
+      </button>
+    </nav>
+  );
 }
